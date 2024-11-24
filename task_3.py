6f0e1f28efcdf1a64a286ac6f33e7d808f5d1530
Yofurr <distrvlad@gmail.com>
2024-11-24 11:00:12 +0300

    Add files via upload

---
+++ task_3.py
@@ -0,0 +1,5 @@
+Number_of_completed_homework_assignments=12
+Number_of_hours_spent=1.5
+Course_name='Python'
+Time_for_one_task=(Number_of_hours_spent/Number_of_completed_homework_assignments)
+print('Курс:', Course_name,", всего задач:", Number_of_completed_homework_assignments, ', затрачено часов:', Number_of_hours_spent, ', среднее время выполнение', Time_for_one_task, 'часа.')
